bebaa6d194e2eeb564d75222bfe71d7cd17ae58e
wherefore <wherefore0@gmail.com>
2025-12-10 11:31:11 -0500

    moving obstacles
    but not respawning

--- project/scripts/obstacle.gd
+++ project/scripts/obstacle.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
+var speed = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	position.x -= speed * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
